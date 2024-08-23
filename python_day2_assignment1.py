7e4c48f7be49564fa50fdbaeee5cf000b4fb9c11
RitikaGupta3150 <166972112+RitikaGupta3150@users.noreply.github.com>
2024-08-23 11:06:59 +0530

    Add files via upload

---
+++ python_day2_assignment1.py
@@ -0,0 +1,11 @@
+#Q1 Calculate the multiplication and sum of two numbers
+num1 = 10
+num2 = 5
+
+Multiplication = num1 * num2
+Addition = num1 + num2
+
+print("Multiplication:", Multiplication)
+print("Sum:", Addition)
+
+#Ans=> Sum:15 and Multiplication: 150
