572a55c1eea3a7d7aa4c32549f670bad6ed5df7d
RitikaGupta3150 <166972112+RitikaGupta3150@users.noreply.github.com>
2024-09-22 11:39:30 +0530

    Add files via upload

---
+++ Python_Day19_NumpyStatisticalFuntions_Assignment2.py
@@ -0,0 +1,15 @@
+"""
+2.Compute the standard deviation of the NumPy array
+Input: arr = [20, 2, 7, 1, 34]
+"""
+
+import numpy as np
+
+arr = np.array([20, 2, 7, 1, 34])
+std_deviation = np.std(arr)
+print(std_deviation)
+
+"""
+ANSWER=
+12.576167937809991
+"""
